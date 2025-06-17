20f22db1a768a2b4186a078f070f023a0dc9b781
Allan <allanrs19@gmail.com>
2025-06-16 23:16:50 -0400

    fix project keys in env

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,17 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDf9zI0_T6JO1Pv_mUDkS45K3hLtzVAWck",
-    authDomain: "arsprepwise.firebaseapp.com",
-    projectId: "arsprepwise",
-    storageBucket: "arsprepwise.firebasestorage.app",
-    messagingSenderId: "9773634009",
-    appId: "1:9773634009:web:e7fe720f5c917f9cf909c8",
-    measurementId: "G-44WJBVVE3J"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
