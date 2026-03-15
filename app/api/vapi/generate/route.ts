ca3b855b00b8729c534c2d91c932db7fa3df3ccf
Allan <allanrs19@gmail.com>
2026-03-15 01:21:29 -0400

    changed ai model using to generate questions

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
 
     try {
         const { text: questions } = await generateText({
-            model: google('gemini-2.5-flash-preview-04-17'),
+            model: google('gemini-2.0-flash-live-001'),
             prompt: `Prepare questions for a job interview.
                 The job role is ${role}.
                 The job experience level is ${level}.
